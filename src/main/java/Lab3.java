91b9eacf429fce1e7f134f49d8cc2cb61f303ae4
gavindya <nuwandijayawardena94@gmail.com>
2018-02-09 09:45:52 +0530

    Changed to get the preffered mean time from user

--- src/main/java/Lab3.java
+++ src/main/java/Lab3.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 import org.apache.commons.math3.distribution.ExponentialDistribution;
 
 import java.util.concurrent.Semaphore;
 
 public class Lab3 {
-    static ExponentialDistribution busTime = new ExponentialDistribution(20 * 60);
-    static ExponentialDistribution riderTime = new ExponentialDistribution(30);
+   static Scanner sc = new Scanner(System.in);
+
+    static ExponentialDistribution busTime;
+    static ExponentialDistribution riderTime;
 
     // define variables
     static int waiting = 0;
@@ -12,8 +15,18 @@ public class Lab3 {
     static Semaphore bus = new Semaphore(0);
     static Semaphore boraded = new Semaphore(0);
 
-
     public static void main(String[] args) {
+        //get preffered time of execution from user
+        System.out.println("Enter 1 to simulate the program at the means of 20 minutes and 30 seconds\n"
+                + "Enter any other integer to simulate the program at the means of 1.2 seconds and 0.03 seconds");
+        int ratio = sc.nextInt();
+        if (ratio == 1) {
+            busTime = new ExponentialDistribution(20 * 60 * 1000);
+            riderTime = new ExponentialDistribution(30 * 1000);
+        } else {
+            busTime = new ExponentialDistribution(20 * 60);
+            riderTime = new ExponentialDistribution(30);
+        }
         // bus creater
         new Thread(new Runnable() {
             public void run() {
@@ -21,7 +34,8 @@ public class Lab3 {
                     try {
                         Thread.sleep((long) Lab3.busTime.sample());
                         new Thread(new Bus()).start();
-                    } catch (InterruptedException e) {}
+                    } catch (InterruptedException e) {
+                    }
                 }
             }
         }).start();
@@ -33,7 +47,8 @@ public class Lab3 {
                     try {
                         Thread.sleep((long) Lab3.riderTime.sample());
                         new Thread(new Rider()).start();
-                    } catch (InterruptedException e) {}
+                    } catch (InterruptedException e) {
+                    }
                 }
             }
         }).start();
